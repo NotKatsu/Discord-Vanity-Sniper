f1c7aef13ed3bb7b943f1a5cc9d0ec9de788cdb5
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-03-26 12:13:14 +0100

    vanity class created

---
+++ helpers/vanity.py
@@ -0,0 +1,40 @@
+import httpx 
+
+from typing import Union
+
+class vanity: 
+    def __init__(self) -> None:
+        pass
+        
+    def guild_id(vanity: str) -> Union[None, str]:
+        result = httpx.get(f"https://canary.discord.com/api/v9/invites/{vanity}")
+        
+        if result.status_code == 200: 
+            return result.json()["guild"]["id"]
+        else:
+            return None
+        
+    def guild_boost_count(vanity: str) -> int:
+        result = httpx.get(f"https://canary.discord.com/api/v9/invites/{vanity}")
+        
+        if result.status_code == 200: 
+            return result.json()["guild"]["premium_subscription_count"]
+        else:
+            return None
+        
+    def guild_name(vanity: str) -> str:
+        result = httpx.get(f"https://canary.discord.com/api/v9/invites/{vanity}")
+        
+        if result.status_code == 200: 
+            return result.json()["guild"]["name"]
+        else:
+            return None
+        
+    def guild_description(vanity: str) -> str: 
+        result = httpx.get(f"https://canary.discord.com/api/v9/invites/{vanity}")
+        
+        if result.status_code == 200: 
+            return result.json()["guild"]["description"]
+        else:
+            return None
+              

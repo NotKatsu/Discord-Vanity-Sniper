afef489f66d8b3e286a2ab5a9765c47ea448c6b4
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-03-26 13:28:08 +0100

    change vanity when it becomes available

--- sniper.py
+++ sniper.py
@@ -10,9 +10,12 @@ else:
     token: str = get_data()["token"]
     debugging: str = get_data()["debugging"]
     vanities: list[str] = get_data()["vanities"]
+    guild_id: int = get_data()["guild_id"]
     
     for vanity in vanities: 
         if vanity_client.vanity_taken(vanity) is True:
             print(f"""{Fore.RED}[VANITY] {vanity.upper()}\n{Fore.RED}[GUILD NAME] {vanity_client.guild_name(vanity)}{Fore.RESET}\n{Fore.RED}[GUILD ID] {vanity_client.guild_id(vanity)}{Fore.RESET}\n{Fore.RED}[BOOST COUNT] {vanity_client.guild_boost_count(vanity)}{Fore.RESET}\n""")
-        
+        else:
+            if vanity_client.change_vanity_code(int(guild_id), vanity, token) == True:
+                exit()
 

011d05f9a3db56c6c154f9479e2893cba1124797
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-03-26 13:28:40 +0100

    add change_vanity_code function

--- helpers/vanity.py
+++ helpers/vanity.py
@@ -1,3 +1,4 @@
+import json
 import httpx 
 
 from typing import Union
@@ -7,7 +8,7 @@ class vanity_client:
         pass
     
     def vanity_taken(vanity: str) -> bool:
-        result = httpx.get(f"https://canary.discord.com/api/v9/invites/{vanity}")
+        result = httpx.get(f"https://discord.com/api/v9/invites/{vanity}")
         
         if result.status_code == 200: 
             return True
@@ -15,7 +16,7 @@ class vanity_client:
             return False
         
     def guild_id(vanity: str) -> Union[None, str]:
-        result = httpx.get(f"https://canary.discord.com/api/v9/invites/{vanity}")
+        result = httpx.get(f"https://discord.com/api/v9/invites/{vanity}")
         
         if result.status_code == 200: 
             return result.json()["guild"]["id"]
@@ -23,7 +24,7 @@ class vanity_client:
             return None
         
     def guild_boost_count(vanity: str) -> int:
-        result = httpx.get(f"https://canary.discord.com/api/v9/invites/{vanity}")
+        result = httpx.get(f"https://discord.com/api/v9/invites/{vanity}")
         
         if result.status_code == 200: 
             return result.json()["guild"]["premium_subscription_count"]
@@ -31,7 +32,7 @@ class vanity_client:
             return None
         
     def guild_name(vanity: str) -> str:
-        result = httpx.get(f"https://canary.discord.com/api/v9/invites/{vanity}")
+        result = httpx.get(f"https://discord.com/api/v9/invites/{vanity}")
         
         if result.status_code == 200: 
             return result.json()["guild"]["name"]
@@ -39,9 +40,27 @@ class vanity_client:
             return None
         
     def guild_description(vanity: str) -> str: 
-        result = httpx.get(f"https://canary.discord.com/api/v9/invites/{vanity}")
+        result = httpx.get(f"https://discord.com/api/v9/invites/{vanity}")
         
         if result.status_code == 200: 
             return result.json()["guild"]["description"]
         else:
             return None
+        
+
+    def change_vanity_code(guild_id: int, vanity: str, token: str) -> bool:
+        try:
+            payload = {'code': vanity}
+            headers = {
+                "Authorization": f"{token}",
+                "Content-Type": "application/json",
+            }
+            
+            response = httpx.patch(f"https://discord.com/api/v9/guilds/{guild_id}/vanity-url", data=json.dumps(payload), headers=headers)
+            
+            if response.status_code == 200: 
+                return True
+            else:
+                return False
+        except:
+            return False
